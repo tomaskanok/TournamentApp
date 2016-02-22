f9cff9307e89d38f645f63e340862555a7e1e1e9
Tomáš Kaňok <tomas.kan@seznam.cz>
2016-02-22 20:25:30 +0100

    Draw maker

---
+++ TournamentApp/Models/MakeDraw.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TournamentApp.Models
+{
+    public class MakeDraw
+    {
+
+        int sizeOfWeb;
+        MakeDraw (List<ApplicationUser> listUsers)
+        {
+            sizeOfWeb = 2;
+            while (listUsers.Count > sizeOfWeb)
+            {
+                sizeOfWeb *= 2;
+            }
+        }
+
+        void mixFightersToFights()
+        {
+
+        }
+
+    }
+}
